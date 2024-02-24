668775b791fab09f2d6e6438a27ac944c0692582
Matteo <matteo.moisant@3mty.team>
2024-02-24 12:16:42 +0100

    [ADD] Swagger docker

---
+++ swagger/website/swagger/assets/js/swagger-initializer.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "https://internal-api.descolar.fr/v1/api/data.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
